ffe5dbb02c41522c1bfcc50c3a095a1868bd9eac
Miguel991 <python_hmc@hotmail.com>
2017-04-19 22:39:05 -0300

    First Commit Spring Boot

---
+++ src/main/java/com/hmc/BackendhmcApplication.java
@@ -0,0 +1,12 @@
+package com.hmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendhmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendhmcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hmc/BackendhmcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hmc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendhmcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
